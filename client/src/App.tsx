dae474786b802176bc304897b99c60f539cf97ed
MrTban <esteman214@gmail.com>
2023-03-14 13:36:05 -0500

    render Home component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,20 @@
-import { useState } from 'react'
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
+import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './components/Home/Home';
 
 function App() {
-  return (
-    <div className="App mt-2 ml-2">
-      <h1>Complex gym</h1>
-      <Routes>
-        <Route path={""} element={""}></Route>
-        <Route path={""} element={""}></Route>
-        <Route path={""} element={""}></Route>
-        <Route path={""} element={""}></Route>
-      </Routes>
-    </div>
-  )
+	return (
+		<div className='App mt-2 ml-2'>
+			<h1>Complex gym</h1>
+			<Routes>
+				<Route path={''} element={''}></Route>
+				<Route path={'/home'} element={<Home />}></Route>
+				<Route path={''} element={''}></Route>
+				<Route path={''} element={''}></Route>
+			</Routes>
+		</div>
+	);
 }
 
-export default App
+export default App;
