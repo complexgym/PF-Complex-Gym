97feb39e65975f99b9353413d0d63d3726ad6b5c
BudaKhan <ezequiellobo259@gmail.com>
2023-03-14 15:57:17 -0300

    en proceso del home landing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './components/Home/Home'
 
 function App() {
   return (
     <div className="App mt-2 ml-2">
-      <h1>Complex gym</h1>
+      <h1 className="text-2xl">Complex gym</h1>
       <Routes>
         <Route path={""} element={""}></Route>
-        <Route path={""} element={""}></Route>
+        <Route path={"/home"} element={<Home />}></Route>
         <Route path={""} element={""}></Route>
         <Route path={""} element={""}></Route>
       </Routes>
