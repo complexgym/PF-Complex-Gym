9e2dcab31fd81e53cc5249ba1bbaa827fd6f842d
nicovon24 <nicovon24@gmail.com>
2023-03-14 10:05:55 -0300

    adding provider redux

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import { Provider } from 'react-redux'
+import store from './redux/store'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </Provider>,
 )
