b68802582063477689c3160581a5c340a8fbae97
MrTban <esteman214@gmail.com>
2023-03-15 15:33:14 -0500

    loginbtn comp implement & conditional render links

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,11 +1,19 @@
 import { Link } from 'react-router-dom';
 import logo from '../../assets/logo/logo.png';
+import { useAuth0 } from '@auth0/auth0-react';
+import LoginBtn from '../LoginBtn/LoginBtn';
 
 export default function Navbar() {
+	const { logout } = useAuth0();
+
+	const { user, isAuthenticated, isLoading } = useAuth0();
+
+	console.log(user);
+
 	return (
-		<div className='fixed z-30 flex flex-row  w-full h-20 bg-black '>
-			<img className=' h-22 ml-40  ' src={logo} alt='' />
-			<div className=' flex flex-row text-white'>
+		<div className='fixed z-30 flex flex-row  w-full h-20 bg-black bg-opacity-90 justify-between '>
+			<img className=' h-22 ml-40  ' src={logo} alt='logo' />
+			<div className=' flex flex-row text-white gap-16 items-center mr-10  '>
 				<Link to='/home'>Inicio</Link>
 				<ul>
 					<li>
@@ -20,19 +28,31 @@ export default function Navbar() {
 				</ul>
 				<Link to='/planes'>Planes</Link>
 
-				<Link to='/contacto'>Contacto</Link>
+				<LoginBtn />
 
-				<ul>
-					<li>
-						<Link to=''>Log In</Link>
-					</li>
-					<li>
-						<Link to='/perfil'>Perfil</Link>
-					</li>
-					<li>
-						<Link to=''>Log Out</Link>
-					</li>
-				</ul>
+				{isAuthenticated && (
+					<div>
+						<img
+							className=' rounded-full max-w-[40px]'
+							src={user.picture}
+							alt={user.name}
+						/>
+						<ul>
+							<li>
+								<Link to='/perfil'>Perfil</Link>
+							</li>
+							<li>
+								<button
+									onClick={() =>
+										logout({ logoutParams: { returnTo: window.location.origin } })
+									}
+								>
+									Log out
+								</button>
+							</li>
+						</ul>
+					</div>
+				)}
 			</div>
 		</div>
 	);
