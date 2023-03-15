7fd0c580aa1b141d0df5f3fa541441dc5e3e09ab
MrTban <esteman214@gmail.com>
2023-03-15 09:22:18 -0500

    Added Auth0Provider

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<App />
+			<Auth0Provider
+				domain='dev-o2lihq5f3ltnq4we.us.auth0.com'
+				clientId='Hzhyp48SllnDorLA7hv50kNPAf9CWsPv'
+				authorizationParams={{
+					redirect_uri: window.location.origin,
+				}}
+			>
+				<App />
+			</Auth0Provider>
 		</BrowserRouter>
 	</React.StrictMode>
 );
