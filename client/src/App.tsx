becac10e00957c0c8663eeae8c0bca2f5b6bf56b
MrTban <esteman214@gmail.com>
2023-03-14 20:15:19 -0500

    Nav route condition

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,23 @@
 import { useState } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import './App.css';
 import Footer from './components/Footer/Footer';
 import Home from './components/Home/Home';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
+	const location = useLocation();
+
 	return (
 		<div className='App'>
+			{location.pathname !== '/' && <Navbar />}
 			<Routes>
 				<Route path={''} element={''}></Route>
 				<Route path={'/home'} element={<Home />}></Route>
 				<Route path={''} element={''}></Route>
 				<Route path={''} element={''}></Route>
 			</Routes>
-      	<Footer/>
+			<Footer />
 		</div>
 	);
 }
