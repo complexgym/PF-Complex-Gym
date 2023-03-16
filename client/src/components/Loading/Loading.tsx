59861991e868c3f592dde9bfa9c317616c612dff
BudaKhan <ezequiellobo259@gmail.com>
2023-03-15 21:30:25 -0300

    creacion del loading Spline

---
+++ client/src/components/Loading/Loading.tsx
@@ -0,0 +1,9 @@
+import Spline from "@splinetool/react-spline"
+
+export default function Loading(){
+  return(
+    <div>
+       <Spline scene="https://prod.spline.design/ffORQphusIoT5k1H/scene.splinecode" />
+    </div>
+  )
+}
