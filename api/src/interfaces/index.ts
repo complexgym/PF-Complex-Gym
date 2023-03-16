a93e73cdbeac5eade8cb741b82f6dde6304f6be8
WarriorDeveloper <seba.iron28@gmail.com>
2023-03-15 23:16:59 -0300

    config gitignore, add interfaces

---
+++ api/src/interfaces/index.ts
@@ -0,0 +1,18 @@
+export interface client {
+    id: string,
+    user: string,
+    password: string,
+    name: string,
+    dni:  number,
+    avatar: string, // ?? AVATAR TEXT
+    height: number,
+    weight: number,
+    age: number,
+    phone: number, 
+    mail: string,
+    adress: string,
+    active: boolean,
+    debt: boolean,
+    debtAmount: number,
+    routine: string
+}
