138650b4286405f4b0dd00677a9463fd04459ab0
AAlejof <scaramouch11@gmail.com>
2023-03-16 14:03:19 -0300

    complete controller

--- api/src/controllers/activities/getActivitiesById.ts
+++ api/src/controllers/activities/getActivitiesById.ts
@@ -1,3 +1,23 @@
-const getActivitiesById = ()=>{}
+import { json } from "body-parser";
+import { Request, Response } from "express";
+import { models } from "../../db";
 
-export default getActivitiesById
+
+const getActivitiesById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    console.log(id)
+    const activity = await models.activities.findAll({
+        where: {id}
+    });
+
+    activity.length ? 
+      res.status(200).json(activity) :
+      res.status(404).send('Activity not found');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send( "Internal server error" );
+  }
+};
+
+export default getActivitiesById;
